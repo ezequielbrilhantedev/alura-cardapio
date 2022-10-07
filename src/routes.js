4c8d294d58a97a05dcc99ef5e964202ecf86e92b
Ezequiel Brilhante <e.ribeiro.brilhante@accenture.com>
2022-10-07 11:59:57 -0300

    render header and menu in routes

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,22 @@
+import Menu from 'components/Menu';
 import Inicio from 'pages/Inicio';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Cardapio from './pages/Cardapio/index';
+import styles from './Routes.module.scss';
 
 export default function AppRouter() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Inicio />} />
-        <Route path="/cardapio" element={<Cardapio />} />
-      </Routes>
-    </Router>
+    <main>
+      <Router>
+        <Menu />
+        <header className={styles.header}>
+          <div className={styles.header__text}>A casa do código e da massa</div>
+        </header>
+        <Routes>
+          <Route path="/" element={<Inicio />} />
+          <Route path="/cardapio" element={<Cardapio />} />
+        </Routes>
+      </Router>
+    </main>
   );
 }
