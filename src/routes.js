5deefc24580214d275476cc3373e5f68f70cce96
Ezequiel Brilhante <ezequielrb7@gmail.com>
2022-10-10 00:03:37 -0300

    create component Sobre

--- src/routes.js
+++ src/routes.js
@@ -1,22 +1,23 @@
-import Menu from 'components/Menu';
-import Inicio from 'pages/Inicio';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Cardapio from './pages/Cardapio/index';
-import styles from './Routes.module.scss';
+import Menu from 'components/Menu'
+import PaginaPadrao from 'components/PaginaPadrao'
+import Inicio from 'pages/Inicio'
+import Sobre from 'pages/Sobre'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Cardapio from './pages/Cardapio/index'
 
 export default function AppRouter() {
   return (
     <main>
       <Router>
         <Menu />
-        <header className={styles.header}>
-          <div className={styles.header__text}>A casa do código e da massa</div>
-        </header>
         <Routes>
-          <Route path="/" element={<Inicio />} />
-          <Route path="/cardapio" element={<Cardapio />} />
+          <Route path="/" element={<PaginaPadrao />}>
+            <Route index element={<Inicio />} />
+            <Route path="/cardapio" element={<Cardapio />} />
+            <Route path="sobre" element={<Sobre />} />
+          </Route>
         </Routes>
       </Router>
     </main>
-  );
+  )
 }
