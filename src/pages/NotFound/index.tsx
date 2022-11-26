59faabd6a8bc8d1a057de6478eb8b11e7737a474
Ezequiel Brilhante <ezequielrb7@gmail.com>
2022-11-26 12:53:14 -0300

    creating page NotFound

---
+++ src/pages/NotFound/index.tsx
@@ -0,0 +1,20 @@
+import styles from './NotFound.module.scss';
+import { ReactComponent as NotFoundImage } from 'assets/not_found.svg';
+import classNames from 'classnames';
+import stylesTema from 'styles/Tema.module.scss';
+
+export default function NotFound() {
+  return (
+    <div
+      className={classNames({
+        [styles.container]: true,
+        [stylesTema.container]: true,
+      })}
+    >
+      <div className={styles.voltar}>
+        <button onClick={() => {}}>{'< Voltar'}</button>
+      </div>
+      <NotFoundImage />
+    </div>
+  );
+}
--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,15 @@
-import Menu from 'components/Menu'
-import PaginaPadrao from 'components/PaginaPadrao'
-import Inicio from 'pages/Inicio'
-import Sobre from 'pages/Sobre'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Cardapio from './pages/Cardapio/index'
+import Menu from 'components/Menu';
+import PaginaPadrao from 'components/PaginaPadrao';
+import Inicio from 'pages/Inicio';
+import Sobre from 'pages/Sobre';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Cardapio from './pages/Cardapio/index';
+import Footer from 'components/Footer';
+import NotFound from 'pages/NotFound';
 
 export default function AppRouter() {
   return (
-    <main>
+    <main className="container">
       <Router>
         <Menu />
         <Routes>
@@ -16,8 +18,10 @@ export default function AppRouter() {
             <Route path="/cardapio" element={<Cardapio />} />
             <Route path="sobre" element={<Sobre />} />
           </Route>
+          <Route path="*" element={<NotFound />} />
         </Routes>
+        <Footer />
       </Router>
     </main>
-  )
+  );
 }
